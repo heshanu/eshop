30ddba1849c7663abc8ebf3d6ccdd52a86705076
heshanu <heshanu97@gmail.com>
2023-04-10 01:02:57 +0530

    Updates

---
+++ precache-manifest.8eb74a72df3c59121ba0d2890633f7e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e0be3559b22786bc531",
+    "url": "/eshop/static/css/main.7d2f30b6.chunk.css"
+  },
+  {
+    "revision": "4e0be3559b22786bc531",
+    "url": "/eshop/static/js/main.4e0be355.chunk.js"
+  },
+  {
+    "revision": "a3e83d269b4ee0699568",
+    "url": "/eshop/static/js/1.a3e83d26.chunk.js"
+  },
+  {
+    "revision": "50a12a1eb9e466af9652",
+    "url": "/eshop/static/js/2.50a12a1e.chunk.js"
+  },
+  {
+    "revision": "9008777d4d0c7d695e52",
+    "url": "/eshop/static/js/runtime~main.9008777d.js"
+  },
+  {
+    "revision": "a652b6457f0e2406ccf570fdbd8574d0",
+    "url": "/eshop/index.html"
+  }
+];
