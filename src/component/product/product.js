b5260db3fbe4872d0e0405b7a9e28b37eddde067
heshanu <heshanu97@gmail.com>
2023-04-09 23:42:44 +0530

    Added props

--- src/component/product/product.js
+++ src/component/product/product.js
@@ -1,14 +1,33 @@
 import React from "react";
 import "./product.css";
 
-function Product() {
+//import material ui star icon
+//for filled star
+import StarIcon from '@mui/icons-material/Star';
+//for outlined star
+import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
+function Product(props) {
     return (
         <div className="product">
-            <div className="product__info">
-                <p>Title</p>
-                <p className="product__price">$30</p>
+        <div className="product__info">
+            <p>{props.title}</p>
+            <p className="product__price">
+                <small>$</small>
+                <strong>{props.price}</strong>
+            </p>
+            <div className="product__rating">
+                {Array(props.rating)
+                .fill()
+                .map((_, i) => (
+                    <p>⭐</p>
+                ))}
             </div>
         </div>
+
+        <img src={props.image} />
+
+        <button onClick={props.addToBasket}>Add to Basket</button>
+    </div>
     )
 }
 
