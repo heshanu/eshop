6ac35b23b302aac87f043bd9618523ae6745e539
heshanu <heshanu97@gmail.com>
2023-04-11 13:46:02 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/eshop/precache-manifest.a6d5570e124949e5aeaa3ac691c3691c.js"
+  "/eshop/precache-manifest.a36c60bca0c92bc607bcd6a7ceb3f04c.js"
 );
 
 workbox.clientsClaim();
